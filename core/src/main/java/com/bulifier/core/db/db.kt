41b86c95fc799abf288131198095933fcc688805
Ilya Gazman <ilya@gazman.dev>
2024-09-24 16:16:01 -0400

    Fixed age case with folder rename

--- core/src/main/java/com/bulifier/core/db/db.kt
+++ core/src/main/java/com/bulifier/core/db/db.kt
@@ -3,6 +3,7 @@
 package com.bulifier.core.db
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.paging.PagingSource
 import androidx.room.ColumnInfo
@@ -295,11 +296,20 @@ interface FileDao {
 
     @Transaction
     suspend fun updateFolderName(oldFolderFile: File, fullPath: String) {
-        val newPath = if(fullPath.trim().startsWith("/")){
-            fullPath.substring(1)
-        }else{
-            fullPath.trim()
+        val newPath = fullPath.replace("[^a-zA-Z0-9_]".toRegex(), "/").trim().run {
+            when {
+                startsWith("/") -> {
+                    fullPath.substring(1)
+                }
+
+                endsWith("/") -> {
+                    fullPath.substring(0, fullPath.length - 1)
+                }
+
+                else -> this
+            }
         }
+
         val oldFolderPath = if (oldFolderFile.path.isNotBlank()) {
             oldFolderFile.path + "/" + oldFolderFile.fileName
         } else {
@@ -309,16 +319,26 @@ interface FileDao {
         val newFileName = newPath.substringAfterLast('/')
         val newFolderPath = newPath.substringBeforeLast('/')
 
+        Log.d(
+            "updateFolderName",
+            """
+                id: ${oldFolderFile.fileId} 
+                old Path: $oldFolderPath 
+                new Path: $newFolderPath 
+                new Name: $newFileName"""
+                .trimIndent()
+        )
+
         val updatedFile = oldFolderFile.copy(fileName = newFileName, path = newFolderPath)
         val oldFolderPathLength = oldFolderPath.length
 
-        updateFile(updatedFile)
         updateChildPathsByIndex(
             oldFolderPath,
             newPath,
             oldFolderPathLength,
             oldFolderFile.projectId
         )
+        updateFile(updatedFile)
         addParentFolders(newFolderPath, oldFolderFile.projectId)
     }
 
@@ -341,7 +361,8 @@ interface FileDao {
         }
     }
 
-    @Query("""
+    @Query(
+        """
     INSERT OR REPLACE INTO files
     SELECT 
         file_id,
@@ -357,7 +378,8 @@ interface FileDao {
     FROM files
     WHERE path LIKE :oldFolderPath || '%' 
     AND project_id = :projectId
-""")
+"""
+    )
     suspend fun updateChildPathsByIndex(
         oldFolderPath: String,
         newFolderPath: String,
